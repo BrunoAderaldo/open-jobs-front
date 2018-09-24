dd661d59cd6888ff4a4545086af506056febad08
Bruno Aderaldo <brunoaderaldo10@gmail.com>
2018-09-24 17:16:46 -0300

    Added route * for render Errros

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
 import Login from './components/Login/Login';
+import Error404 from './Errors/Error404/Error404';
 
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
@@ -11,6 +13,7 @@ ReactDOM.render(
     <Switch>
       <Route path="/" exact={true} component={App} />
       <Route path="/sign-in" component={Login} />
+      <Route path="*" component={Error404} />
     </Switch>
   </BrowserRouter>
   , document.getElementById('root'));
