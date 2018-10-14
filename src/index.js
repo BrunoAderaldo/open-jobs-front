8371d0aa318b6d581ecb010211b08154f4b8b6a2
Bruno Aderaldo <brunoaderaldo10@gmail.com>
2018-10-14 02:45:10 -0300

    Added moment.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import moment from "moment";
 
 import "bootstrap/js/dist/util";
 import "bootstrap/js/dist/dropdown";
@@ -19,6 +20,49 @@ import Profile from "./components/Profile/Profile";
 import Error404 from "./Errors/Error404/Error404";
 
 import registerServiceWorker from "./registerServiceWorker";
+
+moment.locale('pt-br', {
+  months: 'janeiro_fevereiro_março_abril_maio_junho_julho_agosto_setembro_outubro_novembro_dezembro'.split('_'),
+  monthsShort: 'jan_fev_mar_abr_mai_jun_jul_ago_set_out_nov_dez'.split('_'),
+  weekdays: 'domingo_segunda-feira_terça-feira_quarta-feira_quinta-feira_sexta-feira_sábado'.split('_'),
+  weekdaysShort: 'dom_seg_ter_qua_qui_sex_sáb'.split('_'),
+  weekdaysMin: 'dom_2ª_3ª_4ª_5ª_6ª_sáb'.split('_'),
+  longDateFormat: {
+    LT: 'HH:mm',
+    L: 'DD/MM/YYYY',
+    LL: 'D [de] MMMM [de] YYYY',
+    LLL: 'D [de] MMMM [de] YYYY [às] LT',
+    LLLL: 'dddd, D [de] MMMM [de] YYYY [às] LT'
+  },
+  calendar: {
+    sameDay: '[Hoje às] LT',
+    nextDay: '[Amanhã às] LT',
+    nextWeek: 'dddd [às] LT',
+    lastDay: '[Ontem às] LT',
+    lastWeek: function () {
+      return (this.day() === 0 || this.day() === 6) ?
+        '[Último] dddd [às] LT' : // Saturday + Sunday
+        '[Última] dddd [às] LT'; // Monday - Friday
+    },
+    sameElse: 'L'
+  },
+  relativeTime: {
+    future: 'em %s',
+    past: 'há %s',
+    s: 'segundos',
+    m: 'um minuto',
+    mm: '%d minutos',
+    h: 'uma hora',
+    hh: '%d horas',
+    d: 'um dia',
+    dd: '%d dias',
+    M: 'um mês',
+    MM: '%d meses',
+    y: 'um ano',
+    yy: '%d anos'
+  },
+  ordinal: '%dº'
+});
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
