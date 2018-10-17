e67d927641080c951118435eb1dbac19a3ced10d
Bruno Aderaldo <brunoaderaldo10@gmail.com>
2018-10-17 03:36:31 -0300

    Added initial form for change profile details

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -6,6 +6,36 @@ import Sidebar from "../Sidebar/Sidebar";
 import Headerbar from "../Headerbar/Headerbar";
 
 class Settings extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: "",
+      username: "",
+      userbio: ""
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const value = event.target.value;
+    const name = event.target.name;
+
+    this.setState({ [name]: value });
+  }
+
+  async handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      console.log(e);
+    } catch (error) {
+      this.setState({ error: error.response.data.error });
+    }
+  }
+
   render() {
     return (
       <div className="app-view">
@@ -16,13 +46,40 @@ class Settings extends Component {
             <div className="container-fluid">
               <div className="row">
                 <div className="col">
-                  <h2 className="page__title">Settings</h2>
+                  <h2 className="page__title">Configurações</h2>
                 </div>
               </div>
 
               <div className="row">
                 <div className="col">
-                  <h1>Settings</h1>
+                  <div className="card">
+                    <div className="card-body">
+                      <form className="form-horizontal" onSubmit={this.handleSubmit}>
+                        <h4 className="card-title card-title-divider">Detalhes do perfil</h4>
+                        {/* FOTO DO PERFIL, NOME, USERNAME, BIO */}
+
+                        <div className="form-group">
+                          <label htmlFor="name" className="control-label">Nome</label>
+                          <input id="name" type="text" className="form-control" name="name" placeholder="Bruno Aderaldo" required value={this.state.name} onChange={this.handleChange} />
+                        </div>
+
+                        <div className="form-group">
+                          <label htmlFor="username" className="control-label">Seu nome de usuário</label>
+                          <input id="username" type="text" className="form-control" name="username" placeholder="brunoaderaldo" required value={this.state.username} onChange={this.handleChange} />
+                        </div>
+
+                        <div className="form-group">
+                          <label htmlFor="userbio" className="control-label">Bio</label>
+                          <textarea id="userbio" rows="2" className="form-control" name="userbio" placeholder="Design de produto e identidade de marca visual. Clientes: Adobe, Xero, Opera, Tile" value={this.state.userbio} required onChange={this.handleChange}></textarea>
+                        </div>
+
+                        <div className="form-actions">
+                          <button type="button" className="btn btn-link mr-2">Cancelar</button>
+                          <button type="submit" className="btn btn-primary">Salvar</button>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
